6a2d7cf0721caaa4355cc88463f2aceeed86cf93
Frederic Dorn <83763956+algorithminator@users.noreply.github.com>
2026-01-12 18:58:25 +0100

    Update Insert.java

--- src/main/java/solution/Insert.java
+++ src/main/java/solution/Insert.java
@@ -9,9 +9,12 @@ public class Insert {
 
     public int[] atStart(int value){
         var arrLen = this.arr.length;
+        // Create a new array that has one position more than the existing array.
+        // Assign the value to the first position. 
         int [] newArr = new int[arrLen+1];
         newArr[0] = value;
 
+        // Loop through the exisitng array and shift by one position to the right in the new array. 
         for(int i=0;i<arrLen;i++) {
 
             newArr[i+1] = this.arr[i];
@@ -21,9 +24,13 @@ public class Insert {
 
     public int[] atEnd(int value){
         var arrLen = this.arr.length;
+        // Create a new array that has one position more than the existing array.
+        // Assign the value to the last position. 
         int [] newArr = new int[arrLen+1];
         newArr[arrLen] = value;
 
+        // Loop through the exisitng array and insert the values at the same position into the new array.
+        // hmm. can that be made faster?
         for(int i=0;i<arrLen;i++) {
 
             newArr[i] = this.arr[i];
@@ -32,6 +39,8 @@ public class Insert {
     }
 
     public int[] atIndex(int value, int pos){
+        // Create a new array that has one position more than the existing array.
+        
         var arrLen = this.arr.length;
         int [] newArr = new int[arrLen+1];
 
@@ -39,14 +48,16 @@ public class Insert {
         if (pos > arrLen - 1){
             return this.atEnd(value);
         }
-
+        //Loop through the NEW array and insert the values at the same position into the new array... 
         for(int i=0;i<arrLen+1;i++) {
             if (i < pos) {
                 newArr[i] = this.arr[i];
             }
+            //..until your reach the target position to insert the new value..
             else if (i == pos) {
                  newArr[pos] = value;
             }
+            //.. then loop through the remainding NEW array and shift  by one position to the left in the exisiting array. 
             else {
                   newArr[i] = this.arr[i-1];
             }
