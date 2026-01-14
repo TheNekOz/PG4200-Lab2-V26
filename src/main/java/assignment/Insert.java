9178847e809673d81dbc51804b5925ca97acba24
TheNekOz <25076443+TheNekOz@users.noreply.github.com>
2026-01-14 10:57:24 +0100

    Insert: atEnd and atIndex methods

--- src/main/java/assignment/Insert.java
+++ src/main/java/assignment/Insert.java
@@ -7,7 +7,6 @@ public class Insert {
         this.arr = arr;
     }
 
-
     public int[] atStart(int value) {
         int arrLen = this.arr.length;
         int[] startArr = new int[arrLen + 1];
@@ -17,5 +16,30 @@ public class Insert {
         // Probably intended to be done manually through for loop, but arraycopy works and is fast
         // Laziness wins over me at this moment, still burned out with major brainfog from exams.
         // Yell at me if I really should do it manually.
+        // Update: Ok I should have read ahead.
+    }
+
+    public int[] atEnd(int value) {
+        int arrLen = this.arr.length;
+        int[] endArr = new int[arrLen + 1];
+        endArr[endArr.length - 1] = value;
+        System.arraycopy(arr, 0, endArr, 0, arrLen);
+        return endArr;
+    }
+
+    public int[] atIndex(int value, int position) {
+        int arrLen = this.arr.length;
+        int[] indexArr = new int[arrLen + 1];
+        // Ah welp, this forces me to do it manually.
+        for (int i = 0; i < arrLen + 1; i++) {
+            if (i < position) {
+                indexArr[i] = this.arr[i];
+            } else if (i == position) {
+                indexArr[position] = value;
+            } else {
+                indexArr[i] = this.arr[i-1];
+            }
+        }
+        return indexArr;
     }
 }
